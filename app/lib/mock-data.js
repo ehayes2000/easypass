7d98f05385d54987e06c07b73c7af440ff6b3318
unknown <ericqualehayes@gmail.com>
2024-01-06 21:45:45 -0700

    initial commit

---
+++ app/lib/mock-data.js
@@ -0,0 +1,32 @@
+const passwordManagers = [
+    {
+        id: "410544b2-4001-4271-9855-fec4b6a6442a",
+        name: "1Password",
+        license: "proprietary",
+        platforms: ["Android", "IOS", "Linux", "macOS", "Windows"]
+    }
+];
+
+const features = [
+    {  
+        id: "410544b2-4001-4271-9855-fec4b6a6442a",
+        browserImport: true, 
+        competitorImport: true,
+        multiAuth: true,
+        exportData: true,
+        autoPassCapture: true, 
+        autoPassReplay: true,
+        forms: true,
+        multiFormIdentities: true,
+        actionableSecurityReport: true,
+        secureSharing: true,
+        digitalLegacy: true,
+        portableEdition: true,
+        applicationPasswords: true,
+        browserMenuLogins: true,
+        appEncryption: true,
+        securePassSharing: true
+    }
+];
+
+const breaches = [];
